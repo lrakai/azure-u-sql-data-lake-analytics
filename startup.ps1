9336bc5ba2cee72052df1bdaebc730b722e0d5da
Logan Rakai <lmrakai@gmail.com>
2018-03-28 12:54:04 -0600

    Add deployment command on startup

--- startup.ps1
+++ startup.ps1
@@ -2,3 +2,4 @@ $Region = "WestUS2"
 $ResourceGroup = "data-lake-lab"
 Login-AzureRmAccount
 New-AzureRmResourceGroup -Name $ResourceGroup -Location $Region
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile .\infrastructure\arm-template.json -Name "data-lake-lab"
